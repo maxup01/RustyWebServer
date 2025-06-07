79b8f2ca3abeb40a9eff2a9c0f45813a563cc408
Maks Garczynski <rgarczynski66@gmail.com>
2025-06-07 16:54:54 +0200

    Some code placed in get macro has been extracted to  generate_deserialization_block function

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,12 +1,38 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Meta, FnArg, PatType, Pat};
 use quote::{quote, format_ident};
 use std::vec::Vec;
 
+fn generate_deserialization_block(fn_args: &Vec<(syn::Ident, syn::Type)>) -> Vec<TokenStream> {
+    let mut deserialized = vec![];
+
+    for (arg_name, arg_type) in fn_args {
+        let arg_str = arg_name.to_string();
+
+        deserialized.push(quote! {
+            let param_val_orig = map_with_params.get(&#arg_str[..]).unwrap().as_str();
+            let param_val: &str;
+            let formatted;
+
+            if !(param_val_orig.starts_with("{") && param_val_orig.ends_with("}")) {
+                formatted = format!("\"{}\"", param_val_orig);
+                param_val = &formatted;
+            } else {
+                param_val = param_val_orig;
+            }
+
+            let #arg_name: #arg_type = serde_json::from_str(param_val)
+                .expect(&format!("Failed to deserialize argument {}", #arg_str));
+        });
+    }
+
+    deserialized
+}
+
 #[proc_macro_attribute]
-pub fn get(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn get(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let args = parse_macro_input!(args as AttributeArgs);
     let input_fn = parse_macro_input!(input as ItemFn);
 
@@ -45,14 +71,7 @@ pub fn get(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let register_fn_name = format_ident!("register_route_{}", fn_name);
 
-    let mut deserialized_args = vec![];
-    for (arg_name, arg_type) in &fn_args {
-        let arg_str = arg_name.to_string();
-        deserialized_args.push(quote! {
-        let #arg_name: #arg_type = serde_json::from_str(&format!("\"{}\"", map_with_params.get(&#arg_str[..]).unwrap())[..])
-            .expect(&format!("Failed to deserialize argument {}", #arg_str));
-        });
-    }
+    let deserialized_args = generate_deserialization_block(&fn_args);
 
     let expanded = quote! {
         use utils::request::route::{register_route, extract_path_from_request, Method};
@@ -76,5 +95,5 @@ pub fn get(args: TokenStream, input: TokenStream) -> TokenStream {
         }   
     };
 
-    TokenStream::from(expanded)
+    expanded.into()
 }
