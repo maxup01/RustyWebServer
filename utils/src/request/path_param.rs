bf0a4b9fde396b0742b77ce4880d1eddd307a832
Maks Garczynski <rgarczynski66@gmail.com>
2025-06-08 10:24:40 +0200

    Changed one function in path_param module

--- utils/src/request/path_param.rs
+++ utils/src/request/path_param.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap};
+
 pub fn extract_path_param_names_from_path(path: &str) -> Vec<String> {
     let param_names: Vec<String> = path.split('/').filter(|s| s.starts_with('{') && s.ends_with('}'))
         .map(|s| (&s[1..s.len() - 1]).to_string()).collect();
@@ -5,11 +7,11 @@ pub fn extract_path_param_names_from_path(path: &str) -> Vec<String> {
     param_names
 }
 
-pub fn extract_path_param_values_from_path(route_path: &str, path: &str) -> Vec<String> {
+pub fn extract_path_params(route_path: &str, path: &str) -> HashMap<String, String> {
     let route_path_parts: Vec<&str> = route_path.split('/').collect();
     let path_parts: Vec<&str> = path.split('/').collect();
 
-    let mut param_values_as_json: Vec<String> = vec![];
+    let mut param_values_as_json: HashMap<String, String> = HashMap::new();
 
     for (route_path_part, path_part) in route_path_parts.iter().zip(path_parts.iter()) {
         if !route_path_part.starts_with('{') || !route_path_part.ends_with('}') {
@@ -17,9 +19,13 @@ pub fn extract_path_param_values_from_path(route_path: &str, path: &str) -> Vec<
         }
         else if route_path_part != path_part {
             if path_part.starts_with("{") && path_part.ends_with("}") {
-                param_values_as_json.push((&path_part[0..path_part.len()]).to_string());
+                param_values_as_json.insert(
+                    (route_path_part[1..route_path_part.len() - 1]).to_string(),
+                     (&path_part[0..path_part.len()]).to_string());
             } else {
-                param_values_as_json.push(format!("\"{}\"", (&path_part[0..path_part.len()])));
+                param_values_as_json.insert(
+                    route_path_part[1..route_path_part.len() - 1].to_string(),
+                     format!("\"{}\"", (&path_part[0..path_part.len()])));
             }
         }
     }
--- utils/tests/path_param_tests.rs
+++ utils/tests/path_param_tests.rs
@@ -1,4 +1,4 @@
-use utils::request::path_param::{extract_path_param_names_from_path, extract_path_param_values_from_path};
+use utils::request::path_param::{extract_path_param_names_from_path, extract_path_params};
 
 const PATH_WITH_SOME_PATH_PARAMS: &str = "/api/v1/{userId}/posts/{postId}";
 const PATH_WITH_SOME_PATH_PARAMS_FIELD_WITH_VALUES: &str = "/api/v1/10/posts/70";
@@ -13,15 +13,15 @@ pub fn should_return_empty_vec_when_no_path_params() {
 #[test]
 pub fn should_extract_path_param_names_from_path() {
     let path_params = extract_path_param_names_from_path(PATH_WITH_SOME_PATH_PARAMS);
-    assert_eq!(path_params, vec!["userId".to_string(), "postId".to_string()]);
+    assert_eq!(path_params, vec![String::from("userId"), String::from("postId")]);
 }
 
 #[test]
 pub fn should_return_empty_vec_when_path_has_no_path_params() {
 
-    let path_params = extract_path_param_values_from_path(PATH_WITH_NO_PATH_PARAMS, PATH_WITH_NO_PATH_PARAMS);
+    let path_params = extract_path_params(PATH_WITH_NO_PATH_PARAMS, PATH_WITH_NO_PATH_PARAMS);
     let second_path_params = 
-        extract_path_param_values_from_path(PATH_WITH_SOME_PATH_PARAMS_FIELD_WITH_VALUES, PATH_WITH_SOME_PATH_PARAMS);
+        extract_path_params(PATH_WITH_SOME_PATH_PARAMS_FIELD_WITH_VALUES, PATH_WITH_SOME_PATH_PARAMS);
     assert!(path_params.is_empty());
     assert!(second_path_params.is_empty());
 }
@@ -29,7 +29,9 @@ pub fn should_return_empty_vec_when_path_has_no_path_params() {
 #[test]
 pub fn should_extract_path_param_values_from_path_that_has_params() {
 
-    let path_params = extract_path_param_values_from_path(PATH_WITH_SOME_PATH_PARAMS, PATH_WITH_SOME_PATH_PARAMS_FIELD_WITH_VALUES);
-    assert_eq!(path_params, vec!["\"10\"".to_string(), "\"70\"".to_string()]);
+    let path_params = extract_path_params(PATH_WITH_SOME_PATH_PARAMS, PATH_WITH_SOME_PATH_PARAMS_FIELD_WITH_VALUES);
+    assert_eq!(path_params.len(), 2);
+    assert_eq!(path_params.get("userId").unwrap().as_str(), "\"10\"");
+    assert_eq!(path_params.get("postId").unwrap().as_str(), "\"70\"");
 }
 
